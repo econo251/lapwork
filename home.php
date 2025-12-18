2a0ecfa9f57a3a36d397c82ec0b41b06f736139a
econo251 <kimfever123@gmail.com>
2025-12-18 22:57:40 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,13 @@
+<?php
+// home.php
+session_start();
+
+if (!isset($_SESSION["username"])) {
+    header("Location: login.php");
+    exit();
+}
+
+echo "ยินดีต้อนรับ " . $_SESSION["username"];
+?>
+<br>
+<a href="logout.php">ออกจากระบบ</a>
